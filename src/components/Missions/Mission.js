e9f78b565c82bb15751cb588ce96363020a05cf0
enkog <nkirukaogu@gmail.com>
2021-10-06 02:37:07 +0100

    Render missions on the missions page

--- src/components/Missions/Mission.js
+++ src/components/Missions/Mission.js
@@ -1,7 +1,10 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { Table } from 'react-bootstrap';
 import { fetchMissions } from '../../redux/Missions/missionReducer';
+// import TableBody from './TableBody';
 import TableHeader from './TableHeader';
+import '../../css/Table.css';
 
 const Mission = () => {
   const items = useSelector((state) => state.missionsReducer);
@@ -12,16 +15,23 @@ const Mission = () => {
   }, []);
 
   return (
-    <div>
+    <Table responsive striped bordered variant="primary">
       <TableHeader />
       {items.map((mission) => (
-        <div key={mission.id}>
-          <p>{mission.name}</p>
-          <p>{mission.desc}</p>
-        </div>
+        <tbody key={mission.id}>
+          <tr>
+            {Array.from({ length: 1 }).map((index) => (
+              <>
+                <td key={index}>{mission.name}</td>
+                <td key={index}>{mission.desc}</td>
+                <td key={index}>status</td>
+                <td key={index}>actions</td>
+              </>
+            ))}
+          </tr>
+        </tbody>
       ))}
-      hi
-    </div>
+    </Table>
   );
 };
 
--- src/components/Missions/TableHeader.js
+++ src/components/Missions/TableHeader.js
@@ -1,21 +1,16 @@
-import { Table } from 'react-bootstrap';
-import '../../css/Table.css';
-
 const TableHeader = () => (
-  <Table responsive striped bordered hover variant="light">
-    <thead>
-      <tr>
-        {Array.from({ length: 1 }).map((index) => (
-          <>
-            <th key={index}>Missions</th>
-            <th key={index}>Description</th>
-            <th key={index}>Status</th>
-            <th key={index}>Actions</th>
-          </>
-        ))}
-      </tr>
-    </thead>
-  </Table>
+  <thead>
+    <tr>
+      {Array.from({ length: 1 }).map((index) => (
+        <>
+          <th key={index}>Missions</th>
+          <th key={index}>Description</th>
+          <th key={index}>Status</th>
+          <th key={index}>Actions</th>
+        </>
+      ))}
+    </tr>
+  </thead>
 );
 
 export default TableHeader;
