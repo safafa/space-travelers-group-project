a4bc12fd1986146ef64ddb59f9e67dafe86f8a34
unknown <safaaballagh97@gmail.com>
2021-10-06 09:15:48 +0100

    Display resererved rockets on myProfile and add minimal styling

--- src/pages/MyProfile.js
+++ src/pages/MyProfile.js
@@ -1,5 +1,25 @@
-const MyProfile = () => (
-  <div>MyProfile page</div>
-);
+/* eslint-disable react/forbid-prop-types */
+import PropTypes from 'prop-types';
+import '../css/MyProfile.css';
+
+const MyProfile = ({ rockets }) => {
+  const myRockets = rockets.filter((rocket) => rocket.reserved);
+  const list = myRockets.map((rocket) => (<li className="myRocket-name" key={rocket.id}>{rocket.name}</li>));
+  return (
+    <section className="d-flex myprofile-section">
+      <div className="my-div">
+        <h1>My missions</h1>
+      </div>
+      <div className="my-div">
+        <h1>My Rockets</h1>
+        <ul className="myRocket-list">
+          {list}
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+MyProfile.propTypes = { rockets: PropTypes.array.isRequired };
 
 export default MyProfile;
