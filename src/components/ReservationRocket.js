8b352f779fd9946d3081bdcc81414772f0854d18
unknown <safaaballagh97@gmail.com>
2021-10-09 14:16:42 +0100

    Refactor rocket booking

--- src/components/ReservationRocket.js
+++ src/components/ReservationRocket.js
@@ -1,27 +1,17 @@
 import '../css/ReservationRocket.css';
 import { useDispatch, useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
-import { bookRocket, cancelRocket } from '../redux/Rockets/rockets';
+import { bookRocket } from '../redux/Rockets/rockets';
 
 const ReservationRocket = ({ rocketId, reserved }) => {
   const dispatch = useDispatch();
   const rockets = useSelector((state) => state.rocketsReducer);
   const bookBotton = () => {
-    dispatch(bookRocket(rockets, rocketId));
-  };
-  const cancelBotton = () => {
-    dispatch(cancelRocket(rockets, rocketId));
+    dispatch(bookRocket(rockets, rocketId, reserved));
   };
 
   return (
-    <>
-      {!reserved && (
-      <button type="button" className="Reserve-Rocket" onClick={bookBotton}> Reserve Rocket</button>
-      )}
-      {reserved && (
-      <button type="button" className="cancel-rocket" onClick={cancelBotton}> Cancel Rocket</button>
-      )}
-    </>
+    <input type="button" className={!reserved ? 'Reserve-Rocket' : 'cancel-rocket'} value={!reserved ? 'Reserve Rocket' : 'Cancel Rocket'} onClick={bookBotton} />
   );
 };
 
--- src/redux/Rockets/rockets.js
+++ src/redux/Rockets/rockets.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 const GET_ROCKETS = 'spacex/rockets/GET_BOOK';
 const BOOK_ROCKET = 'spacex/rockets/BOOK_ROCKET';
-const CANCEL_ROCKET = 'spacex/rockets/CANCEL_ROCKET';
 
 const initialState = [];
 
@@ -19,20 +18,12 @@ export const getRockets = () => async (dispatch) => {
   });
 };
 
-export const bookRocket = (state, id) => (dispatch) => {
+export const bookRocket = (state, id, reserved) => (dispatch) => {
   const newState = state.map((rocket) => {
     if (rocket.id !== id) return rocket;
-    return { ...rocket, reserved: true };
+    return { ...rocket, reserved: !reserved };
   });
   dispatch({ type: BOOK_ROCKET, newState });
-};
-
-export const cancelRocket = (state, id) => (dispatch) => {
-  const newState = state.map((rocket) => {
-    if (rocket.id !== id) return rocket;
-    return { ...rocket, reserved: false };
-  });
-  dispatch({ type: CANCEL_ROCKET, newState });
 };
 
 const reducer = (state = initialState, action) => {
@@ -40,8 +31,6 @@ const reducer = (state = initialState, action) => {
     case GET_ROCKETS:
       return [...action.rockets];
     case BOOK_ROCKET:
-      return action.newState;
-    case CANCEL_ROCKET:
       return action.newState;
     default:
       return state;
